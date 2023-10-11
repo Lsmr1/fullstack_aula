6cdcc0ea041964a628cf0f207fce5fa297f6c8b8
Leonardo Salinas <39696777+Lsmr1@users.noreply.github.com>
2023-10-10 22:24:33 -0300

    upando o 5

---
+++ 3_HANDLEBARS/5_repeticao/index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+// configurando o handlebars como template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashboard', (req, res) => {
+    const items = [ 'Item a', 'Item b', 'Item c' ]
+
+    res.render('dashboard', { items })
+})
+
+app.get('/', (req, res) => {
+    const usuario = {
+        nome: "Leo",
+        profissao: "Estagiario",
+        Idade: 25
+    }
+
+
+    const auth = true
+
+    res.render('home', { usuario, auth })
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
